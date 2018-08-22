629054519065dee953b03c4bd7c1936892fae0a0
Robert Pirritano <rpirritano@gmail.com>
2018-08-22 15:25:44 -0700

    added componentWillMount

--- src/components/ui/Member.js
+++ src/components/ui/Member.js
@@ -3,12 +3,18 @@ import { Component } from 'react'
 
 class Member extends Component {
 
-render() {
+	componentWillMount() {
+		this.style = {
+			backgroundColor: 'gray'
+		}
+	}
+
+	render() {
 
 		const { name, thumbnail, email, admin, makeAdmin } = this.props
 
     return (
-        <div className="member">
+        <div className="member" style={this.style}>
 					<h1>{name}{(admin ? <FaShield /> : null)}</h1>
 					<a onClick={makeAdmin}>Make Admin</a>
 					<img src={thumbnail} alt="profile picture" />

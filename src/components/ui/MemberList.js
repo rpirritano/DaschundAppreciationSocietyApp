37da14b6a0ed3f5930f93639103385c7fd996a15
Robert Pirritano <rpirritano@gmail.com>
2018-08-22 15:21:40 -0700

    added api now to list users

--- src/components/ui/MemberList.js
+++ src/components/ui/MemberList.js
@@ -6,39 +6,48 @@ class MemberList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            members: [
-            {
-                name: "Joe Wilson",
-                email: "joe.wilson@example.com",
-                thumbnail: "https://randomuser.me/api/portraits/men/53.jpg"
-            },
-            {
-                name: "Stacy Gardner",
-                email: "stacy.gardner@example.com",
-                thumbnail: "https://randomuser.me/api/portraits/women/74.jpg"
-            },
-            {
-                name: "Billy Young",
-                email: "billy.young@example.com",
-                thumbnail: "https://randomuser.me/api/portraits/men/34.jpg"
-            }
-          ]
+            members: [],
+            loading: false
         }
+    }
+    //use componentDidMount for as soon as the app does its first render and fetch the api
+    componentDidMount() {
+      this.setState({loading: true})
+      fetch('https://api.randomuser.me/?mat=US&results=12')
+        .then(response => response.json())
+        .then(json => json.results)
+        .then(members => this.setState({
+          members, //ES6 new way, which is same as members: members
+          loading:false
+        }))
     }
 
     render() {
-      //First: Destructure members to use this state
-      //Secont: map the members array
-        const { members } = this.state
+      //1. Destructure members and loading to use this state
+      //2. map the members array
+      //3. Add visual cue for the loading or the member count (length)
+      //4. Refactor map to grab the api
+        const { members, loading } = this.state
         return (
-            <div className="member-list">
-            	<h1>Society Members</h1>
-              {members.map(
-                (data, i) =>
-                      <Member key={i} onClick={email =>console.log(email)}
-                      {...data} />
-              )}
-            </div>
+          <div className="member-list">
+              <h1>Society Members</h1>
+
+              {(loading) ?
+                  <span>loading...</span> :
+                  <span>{members.length} members</span>
+              }
+
+              {(members.length) ?
+                 members.map(
+                (member, i) =>
+                  <Member key={i}
+                              name={member.name.first + ' ' + member.name.last}
+                              email={member.email}
+                              thumbnail={member.picture.thumbnail}/>
+                 ):
+                 <span>Currently 0 Members </span>
+             }
+          </div>
         )
    }
 }

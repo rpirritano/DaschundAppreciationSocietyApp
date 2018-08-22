2f9fb11f61484a93b396f0cdb3ed7df837256d14
Robert Pirritano <rpirritano@gmail.com>
2018-08-22 14:58:41 -0700

    added member list and changed to view list

--- src/components/ui/MemberList.js
+++ src/components/ui/MemberList.js
@@ -1,14 +1,46 @@
 import { Component } from 'react'
 import fetch from 'isomorphic-fetch'
+import Member from './Member'
 
 class MemberList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            members: [
+            {
+                name: "Joe Wilson",
+                email: "joe.wilson@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/men/53.jpg"
+            },
+            {
+                name: "Stacy Gardner",
+                email: "stacy.gardner@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/women/74.jpg"
+            },
+            {
+                name: "Billy Young",
+                email: "billy.young@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/men/34.jpg"
+            }
+          ]
+        }
+    }
+
     render() {
+      //First: Destructure members to use this state
+      //Secont: map the members array
+        const { members } = this.state
         return (
             <div className="member-list">
             	<h1>Society Members</h1>
+              {members.map(
+                (data, i) =>
+                      <Member key={i} onClick={email =>console.log(email)}
+                      {...data} />
+              )}
             </div>
-        )    
-   }     
+        )
+   }
 }
 
 export default MemberList

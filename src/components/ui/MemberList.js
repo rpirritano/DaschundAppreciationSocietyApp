7e5423d75b1a2624e8d9ce7d3662d34e715219d3
Robert Pirritano <rpirritano@gmail.com>
2018-08-22 15:55:38 -0700

    added update component lifecycle methods

--- src/components/ui/MemberList.js
+++ src/components/ui/MemberList.js
@@ -2,13 +2,18 @@ import { Component } from 'react'
 import fetch from 'isomorphic-fetch'
 import Member from './Member'
 
+//5. Add properties for updating methods, ex: administrators
+
 class MemberList extends Component {
     constructor(props) {
         super(props)
         this.state = {
             members: [],
-            loading: false
+            loading: false,
+            administrators: []
         }
+        this.makeAdmin = this.makeAdmin.bind(this)
+        this.removeAdmin = this.removeAdmin.bind(this)
     }
     //use componentDidMount for as soon as the app does its first render and fetch the api
     componentDidMount() {
@@ -22,11 +27,27 @@ class MemberList extends Component {
         }))
     }
 
+    makeAdmin(email) {
+      const administrators = [
+        ...this.state.administrators,
+        email
+      ]
+      this.setState({administrators})
+    }
+
+    removeAdmin(email) {
+      const administrators = this.state.administrators.filter(
+        adminEmail => adminEmail !== email
+      )
+      this.setState({administrators})
+    }
+
     render() {
       //1. Destructure members and loading to use this state
       //2. map the members array
       //3. Add visual cue for the loading or the member count (length)
       //4. Refactor map to grab the api
+      //6. Add the admin property to the map to handle add/remove admin
         const { members, loading } = this.state
         return (
           <div className="member-list">
@@ -41,9 +62,14 @@ class MemberList extends Component {
                  members.map(
                 (member, i) =>
                   <Member key={i}
+                              admin={this.state.administrators.some(
+                                adminEmail => adminEmail === member.email
+                              )}
                               name={member.name.first + ' ' + member.name.last}
                               email={member.email}
-                              thumbnail={member.picture.thumbnail}/>
+                              thumbnail={member.picture.thumbnail}
+                              makeAdmin={this.makeAdmin}
+                              removeAdmin={this.removeAdmin}/>
                  ):
                  <span>Currently 0 Members </span>
              }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import routes from './routes'
-import MemberList from './components/ui/MemberList'
+import routes from './routes'
 
 window.React = React
 
 render(
-	<MemberList />, 
+	routes,
 	document.getElementById('react-container'))
 
 
 //change member to memberlist
+//then change back to routes
